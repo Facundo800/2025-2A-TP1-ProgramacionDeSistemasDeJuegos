5b7db08a65870965bccac949534facbac90a0c4f
Facundo947 <facurosmino17@gmail.com>
2025-05-09 12:34:52 -0300

    Character

--- Assets/1_ServiceAccess/Scripts/Characters/Character.cs
+++ Assets/1_ServiceAccess/Scripts/Characters/Character.cs
@@ -6,14 +6,16 @@ namespace Excercise1
     public class Character : MonoBehaviour, ICharacter
     {
         [SerializeField] protected string id;
-
+        [SerializeField] protected CharacterService characterservice;
         protected virtual void OnEnable()
         {
-            //TODO: Add to CharacterService. The id should be the given serialized field. 
+            characterservice.TryAddCharacter(id, this);
+                //TODO: Add to CharacterService. The id should be the given serialized field. 
         }
 
         protected virtual void OnDisable()
         {
+            characterservice.TryRemoveCharacter(id);
             //TODO: Remove from CharacterService.
         }
     }
